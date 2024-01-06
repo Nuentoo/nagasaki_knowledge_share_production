f6afa137c4ab48444ce75dd99967f6ebaa33feb8
0cz3 <sumire.hamaguchi@nijibox.co.jp>
2024-01-06 13:31:05 +0900

    イベント jsを追加 タグをクリックしたら紐付く記事にクラス名を付与

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -4,4 +4,23 @@
 /* events export */
 
 export const filterEvents = () => {
+    const tags = document.querySelectorAll(
+        '[data-eventsItem = tag]'
+    );
+    const postTags = document.querySelectorAll(
+        '[data-eventsItem = postTag]'
+    );
+
+    tags.forEach(tag => {
+        tag.addEventListener('click', () => {
+            const tarTag = tagItem.innerHTML;
+            postTags.forEach(postTag => {
+                const tarPostTag = postTag.innerHTML;
+                if(tarPostTag === tarTag){
+                const tarPost = postTag.closest('[data-eventsItem = post]')
+                tarPost.classList.add('events__active');
+                }
+            });
+        });
+    });
 }
