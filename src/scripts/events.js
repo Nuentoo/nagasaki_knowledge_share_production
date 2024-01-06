63f4b6138e1f134301c3999d76cd99ff9c9b591e
0cz3 <sumire.hamaguchi@nijibox.co.jp>
2024-01-06 19:12:00 +0900

    イベント 初期状態ですべての記事を表示、選択した記事以外を非表示

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -7,14 +7,23 @@ export const filterEvents = () => {
     const tags = document.querySelectorAll(
         '[data-eventsItem = tag]'
     );
+    const posts = document.querySelectorAll(
+        '[data-eventsItem = post]'
+    );
     const postTags = document.querySelectorAll(
         '[data-eventsItem = postTag]'
     );
 
     tags.forEach(tag => {
         tag.addEventListener('click', () => {
+            tags.forEach(allTag => {
+                allTag.classList.remove('events__active')
+            });
             tag.classList.add('events__active');
             const tarTag = tag.innerHTML;
+            posts.forEach(post => {
+                post.classList.remove('events__active');
+            });
             postTags.forEach(postTag => {
                 const tarPostTag = postTag.innerHTML;
                 if(tarPostTag === tarTag){
