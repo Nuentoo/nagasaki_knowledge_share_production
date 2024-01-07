7e906851ee53fb8679723cd0e733ccae4cca2cdc
0cz3 <sumire.hamaguchi@nijibox.co.jp>
2024-01-07 22:32:00 +0900

    イベント JSDocを追加

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -4,8 +4,20 @@
 /* events export */
 
 export const filterEvents = () => {
+    /**
+     * データ属性がtagの要素
+     * @type {NodeList}
+     */
     const tags = document.querySelectorAll('[data-eventsItem = tag]');
+    /**
+     * データ属性がpostの要素
+     * @type {NodeList}
+     */
     const posts = document.querySelectorAll('[data-eventsItem = post]');
+    /**
+     * データ属性がpostTagの要素
+     * @type {NodeList}
+     */
     const postTags = document.querySelectorAll('[data-eventsItem = postTag]');
 
     tags.forEach(tag => {
@@ -14,13 +26,25 @@ export const filterEvents = () => {
                 allTag.classList.remove('events__active')
             });
             tag.classList.add('events__active');
+            /**
+             * クリックしたタグのinnerHTMLの文字列
+             * @type {string}
+             */
             const tarTag = tag.innerHTML;
             posts.forEach(post => {
                 post.classList.remove('events__active');
             });
             postTags.forEach(postTag => {
+                /**
+                 * postTagのinnerHTMLの文字列
+                 * @type {string}
+                 */
                 const tarPostTag = postTag.innerHTML;
                 if(tarPostTag === tarTag){
+                /**
+                 * クリックしたタグと紐付くpost
+                 * @type {HTMLElement}
+                 */
                 const tarPost = postTag.closest('[data-eventsItem = post]')
                 tarPost.classList.add('events__active');
                 }
