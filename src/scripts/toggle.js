be8ea59cd6a7fb0a724b05c0c89016a4e734d134
0cz3 <sumire.hamaguchi@nijibox.co.jp>
2024-02-18 16:14:44 +0900

    ヘッダー ハンバーガーメニューの部分を修正

--- src/scripts/toggle.js
+++ src/scripts/toggle.js
@@ -5,10 +5,11 @@
 
 export const hamburgerToggle = () => {
     const hamburger = document.querySelector('[data-headerItem = hamburger]');
-    const navList = document.querySelector('[data-headerItem = "navList"]');
+    const navList = document.querySelector('[data-headerItem = "nav"]');
     $(hamburger).on('click', (e) => {
         const target = e.target;
         $(target).toggleClass('opened');
         $(navList).toggleClass('opened');
+        e.stopPropagation();
     });
 }
