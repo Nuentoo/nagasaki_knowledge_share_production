8f0f7d4d7e55ba86bfabaf1bdc52a05b52cf7793
0cz3 <sumire.hamaguchi@nijibox.co.jp>
2024-02-27 19:10:08 +0900

    ヘッダー ナビゲーションメニューの遷移を設定

--- src/scripts/toggle.js
+++ src/scripts/toggle.js
@@ -6,15 +6,25 @@
 export const hamburgerToggle = () => {
     const hamburger = document.querySelector('[data-headerItem = hamburger]');
     const navList = document.querySelector('[data-headerItem = "nav"]');
-    $(hamburger).on('click', (e) => {
-        const target = e.target;
-        $(target).toggleClass('opened');
-        $(navList).toggleClass('opened');
-        e.stopPropagation();
+    const navItems = document.querySelectorAll('[data-headerItem = "navItems"]');
+    const toggleScrollable = () => {
         if ($("body").css("overflow") === "hidden") {
             $("body").css({height:"", overflow:""});
         } else {
             $("body").css({height:"100%", overflow:"hidden"});
         }
+    }
+    $(hamburger).on('click', (e) => {
+        const target = e.target;
+        $(target).toggleClass('opened');
+        $(navList).toggleClass('opened');
+        e.stopPropagation();
+        toggleScrollable();
     });
+    $(navItems).on("click",() => {
+        $(hamburger).removeClass('opened');
+        $(navList).removeClass('opened');
+        toggleScrollable();
+    });
+
 }
