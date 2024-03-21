7f461e8eb7c672f95cc6f244417199a9a14eec04
0cz3 <sumire.hamaguchi@nijibox.co.jp>
2024-03-21 12:55:50 +0900

    イベント（jQuery） 要素をjQueryで取得

--- src/scripts/events-jquery.js
+++ src/scripts/events-jquery.js
@@ -6,59 +6,50 @@
 export const filterEvents = () => {
     /**
      * データ属性がtagの要素
-     * @type {NodeList}
      */
-    const tags = document.querySelectorAll('[data-eventsItem = tag]');
+    const tags = $('[data-eventsItem = tag]');
     /**
      * データ属性がpostの要素
-     * @type {NodeList}
      */
-    const posts = document.querySelectorAll('[data-eventsItem = post]');
+    const post = $('[data-eventsItem = post]');
     /**
      * データ属性がpostTagの要素
-     * @type {NodeList}
      */
-    const postTags = document.querySelectorAll('[data-eventsItem = postTag]');
+    const postTag = $('[data-eventsItem = postTag]');
 
     // タグボタンを非アクティブ化
     const disableTag = () => {
-        $(tags).removeClass('events__active');
+        tags.removeClass('events__active');
     }
     // 押したタグボタンをアクティブ化
     const activeTag = (target) => {
-        $(target).addClass('events__active');
+        target.addClass('events__active');
     }
     // 記事を非アクティブ化
     const disablePost = () =>{
-        $(posts).removeClass('events__active');
+        post.removeClass('events__active');
     }
     // 押したタグボタンと紐づく記事をアクティブ化
     const activePost = (target) => {
-        postTags.forEach(postTag => {
-            /**
-             * クリックしたタグのinnerHTMLの文字列
-             * @type {string}
-             */
-            const tarTag = target.innerHTML;
-            /**
-             * postTagのinnerHTMLの文字列
-             * @type {string}
-             */
-            const postTagHTML = postTag.innerHTML;
-            if(postTagHTML === tarTag){
-                /**
-                 * クリックしたタグと紐付くpost
-                 * @type {HTMLElement}
-                 */
-                const tarPost = postTag.closest('[data-eventsItem = post]')
-                $(tarPost).addClass('events__active');
-            }
+        /**
+         * クリックしたタグのinnerHTMLの文字列
+         * @type {string}
+         */
+        const tarTag = target.context.innerHTML;
+        /**
+         * innerHTMLがtarTagと同一のpostTag
+         */
+        const tarPostTag = postTag.
+        filter((index, element)=>{
+            return element.innerHTML === tarTag;
         });
+        // tarPostTagの親postをアクティブ化
+        tarPostTag.closest('[data-eventsItem = post]').addClass('events__active');
     }
 
     // タグボタンを押したとき発火
-    $(tags).on('click', (e) => {
-        const target = e.target
+    tags.on('click', (e) => {
+        const target = $(e.target)
         disableTag();
         activeTag(target);
         disablePost();
